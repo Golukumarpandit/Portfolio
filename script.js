b8b6472f0e29899b60f0d8c70a410daa49a96696
Golu Kumar Pandit <panditgolu058@gmail.com>
2024-09-05 22:40:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+<script src="https://kit.fontawesome.com/58a810656e.js" crossorigin="anonymous"></script> 
+    document.addEventListener('DOMContentLoaded', function() {
+        var typeData = new Typed(".role", {
+            strings: [
+                "Full Stack developer",
+                "Web Developer",
+                "UI-UX designer",
+                "Backend Developer",
+                "Coder"
+            ],
+            typeSpeed: 100,
+            backSpeed: 80,
+            backDelay: 1000,
+            loop: true
+        });
+    });
+
